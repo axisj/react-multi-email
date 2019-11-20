8192a8916cc19e999aad0157bd73a7481909d453
jopemachine <jopemachine@naver.com>
2019-11-20 03:47:35 -0800

    Add Functional Component in ReactMultiEmail.tsx

--- src/react-multi-email/ReactMultiEmail.tsx
+++ src/react-multi-email/ReactMultiEmail.tsx
@@ -17,4 +17,39 @@ interface IProps {
   placeholder?: string | React.ReactNode;
 }
 
+const ReactMultiEmail: React.FC<IProps> = props => {
+  const [focused, setFocused] = useState<boolean>(false);
+  const [emails, setEmails] = useState<string[]>([]);
+  const [inputValue, setInputValue] = useState<string>('');
+
+  return (
+    <div
+      className={`${className} ${noClass ? '' : 'react-multi-email'} ${
+        focused ? 'focused' : ''
+      } ${inputValue === '' && emails.length === 0 ? 'empty' : ''}`}
+      style={style}
+      onClick={() => {
+        if (emailInputRef.current) {
+          emailInputRef.current.focus();
+        }
+      }}
+    >
+      {placeholder ? <span data-placeholder>{placeholder}</span> : null}
+      {emails.map((email: string, index: number) =>
+        getLabel(email, index, removeEmail),
+      )}
+      <input
+        ref={emailInputRef}
+        type="text"
+        value={inputValue}
+        onFocus={handleOnFocus}
+        onBlur={handleOnBlur}
+        onChange={handleOnChange}
+        onKeyDown={handleOnKeydown}
+        onKeyUp={handleOnKeyup}
+      />
+    </div>
+  );
+};
+
 export default ReactMultiEmail;
