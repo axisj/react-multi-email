9e4e6284df922f6bb215bf4ccd54a9d76063e36a
lsh23 <shseoul14@gamail.com>
2019-11-28 21:01:30 +0900

    Refactor the interface IProps of ReactMultiEmail
    Divide Interface code to simplify ReactMultiEmail.tsx

--- packages/react-multi-email/ReactMultiEmail.tsx
+++ packages/react-multi-email/ReactMultiEmail.tsx
@@ -1,21 +1,7 @@
 import * as React from 'react';
 import isEmailFn from './isEmail';
 import { useState, createRef } from 'react';
-
-interface IProps {
-  emails?: string[];
-  onChange?: (emails: string[]) => void;
-  noClass?: boolean;
-  validateEmail?: (email: string) => boolean;
-  style?: object;
-  getLabel: (
-    email: string,
-    index: number,
-    removeEmail: (index: number) => void,
-  ) => void;
-  className?: string;
-  placeholder?: string | React.ReactNode;
-}
+import IProps from './IProps';
 
 const ReactMultiEmail: React.FC<IProps> = props => {
   const [focused, setFocused] = useState<boolean>(false);
