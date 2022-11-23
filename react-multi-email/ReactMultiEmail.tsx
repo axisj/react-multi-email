1056c48e3c371bef1a835b24fafd8cb6e93a2e2d
tom <tom@axisj.com>
2022-11-23 18:41:18 +0900

    setup eslint

--- react-multi-email/ReactMultiEmail.tsx
+++ react-multi-email/ReactMultiEmail.tsx
@@ -24,22 +24,23 @@ export interface IReactMultiEmailProps {
   spinner?: () => React.ReactNode;
 }
 
-export function ReactMultiEmail({
-  style,
-  getLabel,
-  className = '',
-  noClass,
-  placeholder,
-  autoFocus,
-  enable,
-  onDisabled,
-  validateEmail,
-  onChange,
-  onChangeInput,
-  onFocus,
-  onBlur,
-  spinner,
-}: IReactMultiEmailProps) {
+export function ReactMultiEmail(props: IReactMultiEmailProps) {
+  const {
+    style,
+    getLabel,
+    className = '',
+    noClass,
+    placeholder,
+    autoFocus,
+    enable,
+    onDisabled,
+    validateEmail,
+    onChange,
+    onChangeInput,
+    onFocus,
+    onBlur,
+    spinner,
+  } = props;
   const emailInputRef = React.useRef<HTMLInputElement>(null);
 
   const [focused, setFocused] = React.useState(false);
@@ -47,108 +48,111 @@ export function ReactMultiEmail({
   const [inputValue, setInputValue] = React.useState('');
   const [spinning, setSpinning] = React.useState(false);
 
-  const findEmailAddress = React.useCallback(async (value: string, isEnter?: boolean) => {
-    let validEmails: string[] = [];
-    let inputValue: string = '';
-    const re = /[ ,;]/g;
-    const isEmail = validateEmail || isEmailFn;
+  const findEmailAddress = React.useCallback(
+    async (value: string, isEnter?: boolean) => {
+      const validEmails: string[] = [];
+      let inputValue: string = '';
+      const re = /[ ,;]/g;
+      const isEmail = validateEmail || isEmailFn;
 
-    const addEmails = (email: string) => {
-      for (let i = 0, l = emails.length; i < l; i++) {
-        if (emails[i] === email) {
-          return false;
+      const addEmails = (email: string) => {
+        for (let i = 0, l = emails.length; i < l; i++) {
+          if (emails[i] === email) {
+            return false;
+          }
         }
-      }
-      validEmails.push(email);
-      return true;
-    };
+        validEmails.push(email);
+        return true;
+      };
 
-    if (value !== '') {
-      if (re.test(value)) {
-        let splitData = value.split(re).filter(n => {
-          return n !== '' && n !== undefined && n !== null;
-        });
+      if (value !== '') {
+        if (re.test(value)) {
+          const splitData = value.split(re).filter(n => {
+            return n !== '' && n !== undefined && n !== null;
+          });
 
-        const setArr = new Set(splitData);
-        let arr = [...setArr];
+          const setArr = new Set(splitData);
+          const arr = [...setArr];
 
-        do {
-          const validateResult = isEmail('' + arr[0]);
+          do {
+            const validateResult = isEmail('' + arr[0]);
 
-          if (typeof validateResult === 'boolean') {
-            if (validateResult) {
-              addEmails('' + arr.shift());
-            } else {
-              if (arr.length === 1) {
-                inputValue = '' + arr.shift();
+            if (typeof validateResult === 'boolean') {
+              if (validateResult) {
+                addEmails('' + arr.shift());
               } else {
-                arr.shift();
+                if (arr.length === 1) {
+                  inputValue = '' + arr.shift();
+                } else {
+                  arr.shift();
+                }
+              }
+            } else {
+              // handle promise
+              setSpinning(true);
+
+              if ((await validateEmail?.(value)) === true) {
+                addEmails('' + arr.shift());
+                setSpinning(false);
+              } else {
+                if (arr.length === 1) {
+                  inputValue = '' + arr.shift();
+                } else {
+                  arr.shift();
+                }
               }
             }
-          } else {
-            // handle promise
-            setSpinning(true);
-
-            if ((await validateEmail!(value)) === true) {
-              addEmails('' + arr.shift());
-              setSpinning(false);
-            } else {
-              if (arr.length === 1) {
-                inputValue = '' + arr.shift();
-              } else {
-                arr.shift();
-              }
-            }
-          }
-        } while (arr.length);
-      } else {
-        if (enable && !enable({ emailCnt: emails.length })) {
-          onDisabled?.();
-          return;
-        }
-
-        if (isEnter) {
-          const validateResult = isEmail(value);
-          if (typeof validateResult === 'boolean') {
-            if (validateResult) {
-              addEmails(value);
-            } else {
-              inputValue = value;
-            }
-          } else {
-            // handle promise
-            setSpinning(true);
-            if ((await validateEmail!(value)) === true) {
-              addEmails(value);
-              setSpinning(false);
-            } else {
-              inputValue = value;
-            }
-          }
+          } while (arr.length);
         } else {
-          inputValue = value;
+          if (enable && !enable({ emailCnt: emails.length })) {
+            onDisabled?.();
+            return;
+          }
+
+          if (isEnter) {
+            const validateResult = isEmail(value);
+            if (typeof validateResult === 'boolean') {
+              if (validateResult) {
+                addEmails(value);
+              } else {
+                inputValue = value;
+              }
+            } else {
+              // handle promise
+              setSpinning(true);
+              if ((await validateEmail?.(value)) === true) {
+                addEmails(value);
+                setSpinning(false);
+              } else {
+                inputValue = value;
+              }
+            }
+          } else {
+            inputValue = value;
+          }
         }
       }
-    }
 
-    setEmails([...emails, ...validEmails]);
-    setInputValue(inputValue);
+      setEmails([...emails, ...validEmails]);
+      setInputValue(inputValue);
 
-    if (validEmails.length) {
-      onChange?.([...emails, ...validEmails]);
-    }
+      if (validEmails.length) {
+        onChange?.([...emails, ...validEmails]);
+      }
 
-    if (inputValue !== inputValue) {
-      onChangeInput?.(inputValue);
-    }
-  }, []);
+      if (inputValue !== inputValue) {
+        onChangeInput?.(inputValue);
+      }
+    },
+    [emails, enable, onChange, onChangeInput, onDisabled, validateEmail],
+  );
 
   const onChangeInputValue = React.useCallback(
     async (value: string) => {
       onChangeInput?.(value);
       await findEmailAddress(value);
     },
-    [emails],
+    [findEmailAddress, onChangeInput],
   );
 
   const removeEmail = React.useCallback(
@@ -161,7 +165,7 @@ export function ReactMultiEmail({
       setEmails(_emails);
       onChange?.(_emails);
     },
-    [emails],
+    [emails, onChange],
   );
 
   const handleOnKeydown = React.useCallback(
@@ -178,22 +182,25 @@ export function ReactMultiEmail({
         default:
       }
     },
-    [emails],
+    [emails.length, removeEmail],
   );
 
-  const handleOnKeyup = React.useCallback(async (e: React.KeyboardEvent<HTMLInputElement>) => {
-    switch (e.key) {
-      case 'Enter':
-      case 'Backspace':
-        await findEmailAddress(e.currentTarget.value, true);
-        break;
-      default:
-    }
-  }, []);
+  const handleOnKeyup = React.useCallback(
+    async (e: React.KeyboardEvent<HTMLInputElement>) => {
+      switch (e.key) {
+        case 'Enter':
+        case 'Backspace':
+          await findEmailAddress(e.currentTarget.value, true);
+          break;
+        default:
+      }
+    },
+    [findEmailAddress],
+  );
 
   const handleOnChange = React.useCallback(
     async (e: React.SyntheticEvent<HTMLInputElement>) => await onChangeInputValue(e.currentTarget.value),
-    [],
+    [onChangeInputValue],
   );
 
   const handleOnBlur = React.useCallback(
@@ -202,13 +209,17 @@ export function ReactMultiEmail({
       await findEmailAddress(e.currentTarget.value, true);
       onBlur?.();
     },
-    [onBlur],
+    [findEmailAddress, onBlur],
   );
 
   const handleOnFocus = React.useCallback(() => {
     setFocused(true);
     onFocus?.();
   }, [onFocus]);
+
+  React.useEffect(() => {
+    setEmails(props.emails ?? []);
+  }, [props.emails]);
 
   return (
     <div
