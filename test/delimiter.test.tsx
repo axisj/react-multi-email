a1a46fd1bf8ca6c3ea9af2aea0674534858911f1
HyeongSeoku <gudtjr3437@naver.com>
2023-07-21 17:08:29 +0900

    test: delimiter custom case

--- test/delimiter.test.tsx
+++ test/delimiter.test.tsx
@@ -39,4 +39,39 @@ describe('ReactMultiEmail delimiter TEST', () => {
     fireEvent.change(inputElem, { target: { value: `${duplicationValue};` } });
     expect(emailsWrapper?.childElementCount).toEqual(MAX_TEST_CASE);
   });
+
+  it('Custom delimiter test', () => {
+    const { getByRole } = render(
+      <ReactMultiEmail
+        delimiter='[&/]'
+        getLabel={(email, index, removeEmail) => (
+          <div data-tag key={index}>
+            <div data-tag-item>{email}</div>
+            <span data-tag-handle onClick={() => removeEmail(index)}>
+              ×
+            </span>
+          </div>
+        )}
+      />,
+    );
+    const inputElem = getByRole('textbox');
+    const emailsWrapper = document.querySelector('.data-labels');
+    const MAX_TEST_CASE = 2;
+
+    for (let i = 0; i < MAX_TEST_CASE; i++) {
+      const testValue = i === 0 ? 'test@gmail.com' : 'test@naver.com';
+      const delimiter = i === 0 ? '&' : '/';
+
+      fireEvent.change(inputElem, { target: { value: testValue } });
+      expect(emailsWrapper?.childElementCount).toEqual(i);
+      fireEvent.change(inputElem, { target: { value: `${testValue}${delimiter}` } });
+      expect(emailsWrapper?.childElementCount).toEqual(i + 1);
+    }
+    // 중복 체크
+    const duplicationValue = 'test@gmail.com';
+    fireEvent.change(inputElem, { target: { value: duplicationValue } });
+    expect(emailsWrapper?.childElementCount).toEqual(MAX_TEST_CASE);
+    fireEvent.change(inputElem, { target: { value: `${duplicationValue}/` } });
+    expect(emailsWrapper?.childElementCount).toEqual(MAX_TEST_CASE);
+  });
 });
