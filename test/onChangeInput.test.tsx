6ded6658d2252cd4088b30f15a6a67eff484c470
HoJin KIM <76891694+hovelopin@users.noreply.github.com>
2023-08-14 14:23:56 +0900

    onChangeInput prop test (#150)
    * test : add to onChangeInput test
    
    * test : onChangeInput test code
    
    * test : onChangeInput test
    
    * test : change onChangeInput logic

---
+++ test/onChangeInput.test.tsx
@@ -0,0 +1,34 @@
+import { cleanup, fireEvent, render } from '@testing-library/react';
+import { ReactMultiEmail } from '../react-multi-email';
+import React from 'react';
+
+afterEach(cleanup);
+
+it('ReactMultiEmail onChangeInput TEST', async () => {
+  const mockhandleChangeInput = jest.fn().mockImplementation(value => value);
+
+  const { getByRole } = render(
+    <ReactMultiEmail
+      onChangeInput={mockhandleChangeInput}
+      getLabel={(email, index, removeEmail) => {
+        return (
+          <div data-tag key={index}>
+            <div data-tag-item>{email}</div>
+            <span data-tag-handle onClick={() => removeEmail(index)}>
+              ×
+            </span>
+          </div>
+        );
+      }}
+    />,
+  );
+
+  const input = getByRole('textbox') as HTMLInputElement;
+
+  fireEvent.change(input, { target: { value: 'kimhojin3714@naver.com' } });
+
+  await Promise.resolve();
+
+  expect(mockhandleChangeInput).toHaveBeenCalled();
+  expect(mockhandleChangeInput).toHaveBeenCalledWith('kimhojin3714@naver.com');
+});
