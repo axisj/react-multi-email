51107de953f8921f40cd6fcfb8f7c8c75dd17ddb
vinyl810 <19thsummer@kaist.ac.kr>
2023-09-20 11:11:54 +0000

    test: add test case with no prop

--- test/className.test.tsx
+++ test/className.test.tsx
@@ -24,4 +24,23 @@ describe('ReactMultEmail className TEST', () => {
     const el: HTMLElement | null = document.querySelector('div.react-multi-email');
     expect(el?.className.includes('ef3a7c')).toBe(true);
   });
+  it('className is in default when prop is not provided.', async () => {
+    render(
+      <ReactMultiEmail
+        getLabel={(email, index, removeEmail) => {
+          return (
+            <div data-tag key={index}>
+              <div data-tag-item>{email}</div>
+              <span data-tag-handle onClick={() => removeEmail(index)}>
+                ×
+              </span>
+            </div>
+          );
+        }}
+      />,
+    );
+    const el: HTMLElement | null = document.querySelector('div.react-multi-email');
+    const regex = new RegExp(`^( )*(react-multi-email( )*empty|focused){1}( )*$`);
+    expect(regex.test(el?.className ?? '')).toBe(true);
+  });
 });
