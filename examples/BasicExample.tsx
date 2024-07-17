3a58b583115bee7a4342f75da6f65e029a8154ca
JANG KI-YOUNG <tom@axisj.com>
2024-07-17 18:39:23 +0900

    add inputValue prop (#177)

--- examples/BasicExample.tsx
+++ examples/BasicExample.tsx
@@ -2,15 +2,19 @@ import * as React from 'react';
 import styled from '@emotion/styled';
 import { ReactMultiEmail } from '../react-multi-email';
 import { Button } from 'antd';
+import { useRef, useState } from 'react';
 
 interface Props {}
 
 function BasicExample(_props: Props) {
   const [emails, setEmails] = React.useState<string[]>([]);
   const [focused, setFocused] = React.useState(false);
+  const [input, setInput] = useState<string>('');
+
+  const ref = useRef<HTMLDivElement>(null);
 
   return (
-    <Container>
+    <Container ref={ref}>
       <form>
         <h3>Email</h3>
         <ReactMultiEmail
@@ -21,7 +25,10 @@ function BasicExample(_props: Props) {
           }}
           autoFocus={true}
           onFocus={() => setFocused(true)}
-          onBlur={() => setFocused(false)}
+          onBlur={() => {
+            setFocused(false);
+            setInput('');
+          }}
           onKeyDown={evt => {
             console.log(evt);
           }}
@@ -38,8 +45,10 @@ function BasicExample(_props: Props) {
               </div>
             );
           }}
+          inputValue={input}
           onChangeInput={value => {
             console.log(value);
+            setInput(value);
           }}
         />
         <br />
